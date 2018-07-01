6b77efd8e8e2c433f85786296d5661e45277a3ca
Anil Soni <anky.soni8460@gmail.com>
2018-07-01 19:41:45 +0530

    minor fix

--- GetFiles.py
+++ GetFiles.py
@@ -1,11 +1,10 @@
-from os import chdir
-from os import getcwd
-from glob import glob
+import os
+import glob
 
 class GetFiles:
-    def __init__(self,directory):
+    def __init__(self, directory):
         self._directory = directory
 
     def get_file(self):
-        chdir(self._directory)
-        return getcwd(),glob("*.py")
+        os.chdir(self._directory)
+        return os.getcwd(), glob.glob("*.py")
